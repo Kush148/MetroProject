620b8d7b944e29d1b42b68c3808374302dbba947
kushal <kushal148@live.com>
2019-05-14 16:47:08 -0400

    Updated Driver class implementation

--- src/LetsGo.java
+++ src/LetsGo.java
@@ -4,16 +4,21 @@ public class LetsGo {
     public static void main(String[] args) {
         int noOfMetroStations;
         int retries = 0;
+        int passOnTrain, passOffTrain, newPass;
 
         System.out.println("Welcome To LetsGo Transport");
-        int checkStations;
+
         Scanner userInput = new Scanner(System.in);
 
-        do {
-            System.out.println("Please enter the total number of Metro Stations (Must be >3)");
-            checkStations = userInput.nextInt();
+        do { //for valid number of Stations
+            System.out.println("Please enter the total number of Metro Stations (Must be >=3)");
+            noOfMetroStations = userInput.nextInt();
+        } while (noOfMetroStations < 3);
 
-        } while (checkStations < 3);
+        Train train = new Train();
+        newPass = (int) (Math.random() * ((300 - 1) + 1)) + 1; //(Math.random() * ((max - min) + 1)) + min
+        passOffTrain = (int) (Math.random() * ((300 - 1) + 1)) + 1;
+        passOnTrain = newPass - passOffTrain;
 
     }
 }
